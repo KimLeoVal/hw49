80493cd71cf289a7b7d3e419739c485f8f81ae26
KimLeoVal <kimleo@mail.ru>
2022-07-13 01:10:00 +0600

    добавил в форму widget=forms.CheckboxSelectMultiple

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,12 +3,13 @@ from pyexpat import model
 
 from django import forms
 from django.forms import widgets
+from select import select
 
 from webapp.models import Task, TaskType, TaskStatus
 
 
 class TaskForm(forms.ModelForm):
-    type = forms.ModelMultipleChoiceField(queryset=TaskType.objects.all())
+    type = forms.ModelMultipleChoiceField(queryset=TaskType.objects.all(),widget=forms.CheckboxSelectMultiple)
     status = forms.ModelChoiceField(queryset=TaskStatus.objects.all())
     class Meta:
         model = Task
