13e4468fc1f602dbd2f0d700b3c13d2c4d79bd1c
KimLeoVal <kimleo@mail.ru>
2022-07-14 23:46:51 +0600

    добавил собственные валидаторы для полей type and status

--- webapp/CustomValidators.py
+++ webapp/CustomValidators.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from webapp.models import STATUSES
 
 SP_CHARS = ['!','@','#','$']
 SP_WORDS = ['kill','bomb', 'terrorist', 'president']
@@ -12,3 +13,17 @@ def special_words(text):
     for i in SP_WORDS:
         if i.lower() in text.lower():
             raise ValidationError(f'Description must not contain "{i}!!!!!!". Forbidden words{SP_WORDS}')
+
+def check_count(list):
+    if len(list) > 3:
+        raise ValidationError(f'Values "type" cannot be more than 3')
+
+def check_status(status):
+    list_st = []
+    for i in STATUSES:
+        list_st.append(i[0])
+    list_st = (', ').join(list_st)
+    if status in STATUSES:
+        raise ValidationError(f'"status" value must be "{list_st}". ')
+
+
--- webapp/forms.py
+++ webapp/forms.py
@@ -1,24 +1,19 @@
 from pyexpat import model
 
-
 from django import forms
 from django.core import validators
 
-from CustomValidators import special_chars, special_words
+from CustomValidators import special_chars, special_words, check_count, check_status
 from webapp.models import Task, TaskType, TaskStatus
 
 
 class TaskForm(forms.ModelForm):
-    title = forms.CharField(max_length=20,validators=(special_chars,))
-    description = forms.CharField(max_length=2000,validators=(special_words,), widget = forms.Textarea)
-    type = forms.ModelMultipleChoiceField(queryset=TaskType.objects.all(),widget=forms.CheckboxSelectMultiple)
-    status = forms.ModelChoiceField(queryset=TaskStatus.objects.all())
+    title = forms.CharField(max_length=20, validators=(special_chars,))
+    description = forms.CharField(max_length=2000, validators=(special_words,), widget=forms.Textarea)
+    type = forms.ModelMultipleChoiceField(queryset=TaskType.objects.all(), widget=forms.CheckboxSelectMultiple,
+                                          validators=(check_count,))
+    status = forms.ModelChoiceField(queryset=TaskStatus.objects.all(), validators=(check_status,))
+
     class Meta:
         model = Task
         fields = '__all__'
-
-
-
-
-
-
