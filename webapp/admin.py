55a35ca272455dfdc4350cfbf6975787842507c6
KimLeoVal <kimleo@mail.ru>
2022-07-26 00:02:07 +0600

    перезаписал фикстуры

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from webapp.models import Task, TaskType, TaskStatus
+from webapp.models import Task, TaskType, TaskStatus, Project
 
 admin.site.register(Task)
 admin.site.register(TaskType)
 admin.site.register(TaskStatus)
+admin.site.register(Project)
 
--- webapp/models.py
+++ webapp/models.py
@@ -5,29 +5,34 @@ STATUSES = [('New', 'новый'), ('In Progress', 'в процессе'), ('Don
 
 
 # Create your models here.
-class BaseModel(models.Model ):
-    created_ad = models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
+class BaseModel(models.Model):
+    created_ad = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_ad = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
     class Meta:
-        abstract =True
+        abstract = True
+
 
 class Task(BaseModel):
     title = models.CharField(max_length=20)
     description = models.TextField(max_length=2000, null=True, blank=True, )
     status = models.ForeignKey('webapp.TaskStatus', related_name='statuses', on_delete=models.CASCADE,
                                verbose_name='Статус')
-    type = models.ManyToManyField('webapp.TaskType',related_name='types',blank=True)
+    type = models.ManyToManyField('webapp.TaskType', related_name='types', blank=True)
+    project = models.ForeignKey('webapp.Project', related_name='project', on_delete=models.CASCADE,
+                                verbose_name='Проэкт')
+
     def __str__(self):
         return f'{self.title}'
 
-
     class Meta:
-        db_table="Task"
-        verbose_name='Задача'
-        verbose_name_plural='Задачи'
+        db_table = "Task"
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
 
 class TaskType(models.Model):
-    type = models.CharField(max_length=30,choices=TYPES)
+    type = models.CharField(max_length=30, choices=TYPES)
 
     def __str__(self):
         return f'{self.type}'
@@ -37,8 +42,9 @@ class TaskType(models.Model):
         verbose_name = 'Тип'
         verbose_name_plural = 'Типы'
 
+
 class TaskStatus(models.Model):
-    status = models.CharField(max_length=30,choices=STATUSES)
+    status = models.CharField(max_length=30, choices=STATUSES)
 
     def __str__(self):
         return f'{self.status}'
@@ -48,8 +54,16 @@ class TaskStatus(models.Model):
         verbose_name = 'Статус'
         verbose_name_plural = 'Статусы'
 
+
 class Project(models.Model):
     start = models.DateField(verbose_name='Дата начала')
-    end = models.DateField(null=True,verbose_name = 'Дата окончания')
-    title = models.CharField(max_length=30,verbose_name='Название')
-    description = models.TextField(max_length=2000,verbose_name='Описание')
+    end = models.DateField(null=True, verbose_name='Дата окончания')
+    title = models.CharField(max_length=30, verbose_name='Название')
+    description = models.TextField(max_length=2000, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Проэкт'
+        verbose_name_plural = 'Проэкты'
